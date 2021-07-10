8e6109866e03907c2bc4c0011c7e972021eb0a34
gaiyi7788 <1095671343@qq.com>
2021-07-10 16:58:53 +0800

    week1

---
+++ Week 1/pytorch_learning/test_cnn.py
@@ -0,0 +1,63 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))]
+)
+
+testset = torchvision.datasets.CIFAR10(root='./data',train=False,download=True,
+                                        transform=transform)
+testloader = torch.utils.data.DataLoader(testset,batch_size=4,shuffle=False,num_workers=2)
+
+classes = ('plane','car','bird','cat','deer','dog','frog','horse','ship','truck')
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net,self).__init__()
+        self.conv1 = nn.Conv2d(3,6,5)
+        self.pool = nn.MaxPool2d(2,2)
+        self.conv2 = nn.Conv2d(6,16,5)
+        self.fc1 = nn.Linear(16*5*5,120)
+        self.fc2 = nn.Linear(120,84)
+        self.fc3 = nn.Linear(84,10)
+
+    def forward(self,x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1,16*5*5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net()
+
+PATH = './cifar_net.pth'
+net.load_state_dict(torch.load(PATH))
+
+dataiter = iter(testloader)
+images,labels = dataiter.next()
+
+print('GroundTruch:', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+outputs = net(images)
+_, predicted = torch.max(outputs,1)
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _,predicted = torch.max(outputs.data,1)
+        total = total+labels.size(0)
+        correct = correct + (predicted==labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (100*correct/total))
