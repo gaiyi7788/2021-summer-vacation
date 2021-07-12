6a195aa694ba1e2d166878bb152fcdd9e9ed91f5
gaiyi7788 <1095671343@qq.com>
2021-07-12 15:14:11 +0800

    2021.7.12-15:13

--- gitcommit.sh
+++ gitcommit.sh
@@ -2,16 +2,38 @@ echo 开始添加变更：git add .
 git add .
 echo;
 
-echo 将暂存区内容添加到本地仓库：git commit 
-git commit -m $1
+echo 查看暂存区状态：git status 
+git status
 echo;
 
-read -r -p "是否将变更情况提交到远程自己分支 [y/n]" input
+read -r -p "是否将将暂存区内容添加到本地仓库 [y/n]" input
  
 case $input in
     [yY][eE][sS]|[yY])
-        echo 提交到远程自己分支中：git push -u origin master
-        git push -u origin master
+        echo 暂存区内容添加到本地仓库中：git commit 
+        git commit -m $1
+        echo;
+        
+        read -r -p "是否将变更情况提交到远程自己分支 [y/n]" input
+ 
+        case $input in
+            [yY][eE][sS]|[yY])
+                echo 提交到远程自己分支中：git push -u origin master
+                git push -u origin master
+                echo;
+        exit 1
+        ;;
+
+            [nN][oO]|[nN])
+                echo "中断提交"
+                exit 1
+                ;;
+
+            *)
+            echo "输入错误，请重新输入"
+            ;;
+        esac
+        
         exit 1
         ;;
 
